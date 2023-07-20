bce4cfa8c104f7ae33aee7579bc6159e47d0786b
Oguz Mumcu <oguzmum@gmail.com>
2023-07-20 12:59:32 +0200

    würfel

---
+++ wuerfel_sim.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::time;
+using std::srand; 
+
+
+int main()
+{
+    unsigned short int eingabe; 
+
+    do 
+    {   
+        do
+        {
+            cout << "\nWollen Sie würfeln ? \n 0: Nein \n 1: Ja \n Hier eingeben:";
+            cin >> eingabe;
+
+        } while (eingabe != 1);
+        
+        
+        if (eingabe == 1)
+        {
+            srand(time(0));
+            int zufallszahl = rand() % 6 + 1;
+            cout << "\nGewürfelt: " << zufallszahl << endl; 
+        }
+
+    }while(eingabe == 1);
+}
