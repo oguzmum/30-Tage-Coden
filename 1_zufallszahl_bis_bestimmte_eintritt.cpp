6d9d4dddb71a22fcd6d82904c1bf69ee1219944f
Oguz Mumcu <oguzmum@gmail.com>
2023-07-21 22:14:20 +0200

    verbesserungen

--- 1_zufallszahl_bis_bestimmte_eintritt.cpp
+++ 1_zufallszahl_bis_bestimmte_eintritt.cpp
@@ -12,10 +12,10 @@ using std::srand;
 int main()
 {
     int number_to_be;
-    cout << "Welche Zahl willst du erreichen (0 bis 5): "; 
+    cout << "Welche Zahl willst du erreichen (0 bis 4): "; 
     cin >> number_to_be;
     int count = 0;
-    int zufallszahl = -237147203; //einfach iwas genommen was numbertobe nicht sein kann :) bzw. sollte
+    int zufallszahl;
 
     do 
     {
@@ -23,7 +23,7 @@ int main()
         // hiermit sicherstellen, dass bei jedem mal neue Zufallszahlen generiert werden
         srand(time(0));
 
-        // Zufallszahl zwischen 0 und 5 generieren
+        // Zufallszahl zwischen 0 und 4 generieren
         zufallszahl = rand() % 5;
         //cout << zufallszahl << "\n";
 
